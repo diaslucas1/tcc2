34d8c299908fe68f787b3cb7562d247904986b7a
Lucas Dias <lucasdiasrodrigues@hotmail.com>
2019-08-10 05:19:24 -0300

    Implementing listing, update and deleting registers

--- website/urls.py
+++ website/urls.py
@@ -1,4 +1,4 @@
-from website.views import IndexTemplateView, PropriedadeCreateView
+from website.views import IndexTemplateView, PropriedadeCreateView, PropriedadeListView, PropriedadeUpdateView, PropriedadeDeleteView
 
 from django.urls import path
 
@@ -11,4 +11,13 @@ urlpatterns = [
   # GET /propriedade/cadastrar
   path('propriedade/cadastrar', PropriedadeCreateView.as_view(),
   name="cadastra_propriedade"),
+
+   # GET /propriedades
+  path('propriedades/', PropriedadeListView.as_view(), name="lista_propriedades"),
+
+  # GET/POST /propriedade/{pk}
+  path('propriedade/<pk>', PropriedadeUpdateView.as_view(), name="atualiza_propriedade"),
+
+  # GET/POST /propriedades/excluir/{pk}
+  path('propriedade/excluir/<pk>', PropriedadeDeleteView.as_view(), name="deleta_propriedade"),
 ]
--- website/views.py
+++ website/views.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, ListView, UpdateView, DeleteView
 from website.models import Propriedade
 from website.forms import InserePropriedadeForm
 
@@ -19,3 +19,24 @@ class PropriedadeCreateView(CreateView):
   template_name = "cadastro.html"
   model = Propriedade
   form_class = InserePropriedadeForm
+
+# LISTA PROPRIEDADES
+class PropriedadeListView(ListView):
+  template_name = "lista.html"
+  model = Propriedade
+  context_object_name = "propriedades"
+
+# ATUALIZA PROPRIEDADE
+class PropriedadeUpdateView(UpdateView):
+  template_name = "atualiza.html"
+  model = Propriedade
+  fields = '__all__'
+  context_object_name = 'propriedade'
+  success_url = reverse_lazy("website:lista_propriedades")
+
+# EXCLUI PROPRIEDADE
+class PropriedadeDeleteView(DeleteView):
+  template_name = "exclui.html"
+  model = Propriedade
+  context_object_name = 'propriedade'
+  success_url = reverse_lazy("website:lista_propriedades")
