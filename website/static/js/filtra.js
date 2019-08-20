018f4e2cf78479d55e429cbf1460eb234d2cfce3
Lucas Dias <lucasdiasrodrigues@hotmail.com>
2019-08-20 00:00:13 -0300

    Adding search box to property list

---
+++ website/static/js/filtra.js
@@ -0,0 +1,25 @@
+let searchFilter = document.querySelector(".buscar-propriedade");
+
+searchFilter.addEventListener("keyup", function() {
+  let propriedades = document.querySelectorAll(".propriedade");
+
+  if (this.value.length > 0) {
+    for(let i = 0; i < propriedades.length; i++) {
+      let propriedade = propriedades[i];
+      let tdNome = propriedade.querySelector(".nome-list");
+      let nome = tdNome.textContent;
+      let expressao = new RegExp(this.value, "i");
+
+      if(!expressao.test(nome)) {
+        propriedade.classList.add("hide");
+      } else {
+        propriedade.classList.remove("hide");
+      }
+    }
+  } else {
+    for (let i = 0; i < propriedades.length; i++) {
+      let propriedade = propriedades[i];
+      propriedade.classList.remove("hide");      
+    }
+  }
+});
